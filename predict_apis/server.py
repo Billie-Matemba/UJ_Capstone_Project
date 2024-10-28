075f7377a3fcaab2c2fe746b95b0fb18b0770022
Billie-Matemba <billiematemba133@gmail.com>
2024-10-28 22:18:08 +0200

    deploy2

--- predict_apis/server.py
+++ predict_apis/server.py
@@ -38,6 +38,6 @@ def predict():
     
     return jsonify(prediction)
 
-# if __name__ == '__main__':
-    # app.run(debug=True, port=5000, host='0.0.0.0')
-    # print('')
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
+    print('')
